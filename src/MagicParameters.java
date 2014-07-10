23bedab877d5099e86ec1364f9ee2bcf0525ae3d
Richard Wallace <rich.wallace@pandorabots.com>
2014-07-10 14:39:45 -0400

    moved parameters to config.txt

---
+++ src/MagicParameters.java
@@ -0,0 +1,6 @@
+/**
+ * Created by User on 7/10/2014.
+ */
+public class MagicParameters {
+    public static String version = "0.0.1";
+}
--- src/MagicStrings.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by User on 7/10/2014.
- */
-public class MagicStrings {
-    public static String version = "0.0.1";
-}
