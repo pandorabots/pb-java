0b56c987976298fd43025479093d2bcd973ac376
Richard Wallace <rich.wallace@pandorabots.com>
2014-07-10 11:29:14 -0400

    created project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by User on 7/10/2014.
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        System.out.println("Pandorabots API Java SDK version "+MagicStrings.version);
+    }
+}
